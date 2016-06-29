4e57766baa88ed5d71193f62134638234e62a573
Joe Gertler <jojogertler@gmail.com>
2016-06-29 14:49:46 -0700

    gravity generation gist
    A flowchart of how gravity generation will work.

---
+++ Gists/gravitygen.js
@@ -0,0 +1,14 @@
+#pragma strict
+import random.js
+//something along the lines of random, we will decide how we want to generate settings later.
+function Start () {
+//on start, generates a new gravity setting
+//declare what variable gravity is(based on the dungeon)
+//declare the force of each 'side' of gravity is
+//output a string to the player telling them to 'watch out, gravity is to the 'certain direction' this time'
+//rotate the sprite in order to match the direction of gravity
+}
+
+function Update () {
+/
+}
